a35034596a7dfd4b903cbe02e92cdc90a1913067
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-04-02 13:52:02 -0300

    first configurations

---
+++ src/test/java/com/myproject/desafio_picpay_backend/DesafioPicpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.desafio_picpay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
