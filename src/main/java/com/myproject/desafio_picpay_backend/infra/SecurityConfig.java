9a4066df54c90ef65cd43dd8aafea1d2860e7fa0
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-04-04 15:02:53 -0300

    creating password criptografy

---
+++ src/main/java/com/myproject/desafio_picpay_backend/infra/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.myproject.desafio_picpay_backend.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        return http
+                .csrf(csfr -> csfr.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(form -> form.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .build();
+    }
+
+}
--- src/main/java/com/myproject/desafio_picpay_backend/services/UserService.java
+++ src/main/java/com/myproject/desafio_picpay_backend/services/UserService.java
@@ -4,6 +4,7 @@ import com.myproject.desafio_picpay_backend.dtos.UserDTO;
 import com.myproject.desafio_picpay_backend.models.User;
 import com.myproject.desafio_picpay_backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,12 +15,17 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void saveUser(User user){
         this.userRepository.save(user);
     }
 
     public User createUser(UserDTO dto){
         User newUser = new User(dto);
+
+        encodePassword(newUser);
 
         saveUser(newUser);
 
@@ -34,4 +40,11 @@ public class UserService {
         return this.userRepository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
     }
 
+    public void encodePassword(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+    }
+
+    public boolean validatePassword(String passwordForm, String passwordDatabase){
+        return passwordEncoder.matches(passwordForm, passwordDatabase);
+    }
 }
